6e00b2c65284993f17e7e7ab77058cdc546097f4
Harrison Curtis <harrisoncurtis50@gmail.com>
2024-10-22 22:57:12 +0100

    Stan conda install bash script

---
+++ stan_install.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+conda create -n stan_env -y
+conda activate stan_env 
+conda install -c conda-forge numpy patsy scipy pandas matplotlib seaborn arviz jupyter numba -y
+conda install -c conda-forge nutpie -y
+conda install -c conda-forge cmdstanpy -y
