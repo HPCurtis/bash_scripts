eddef605e7b11943d92331b821afc5b0e08f747e
Harrison Curtis <harrisoncurtis50@gmail.com>
2024-10-22 22:16:30 +0100

    pymc conda install script

---
+++ pymc_install.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+conda create -n pymc_env python=3.11 -y
+conda activate pymc_env 
+conda install -c conda-forge numba numpy pandas matplotlib seaborn arviz pytensor "pymc>=5" -y
+conda install numpyro -y
+conda install -c conda-forge nutpie -y
