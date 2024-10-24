01e8c432305b2c90254f5f45d1380101a59b54b2
Harrison Curtis <harrisoncurtis50@gmail.com>
2024-10-24 17:49:13 +0100

    Update to pptmc install script

--- pymc_install.sh
+++ pymc_install.sh
@@ -2,6 +2,6 @@
 
 conda create -n pymc_env python=3.11 -y
 conda activate pymc_env 
-conda install -c conda-forge numba numpy pandas matplotlib seaborn arviz pytensor "pymc>=5" -y
+conda install -c conda-forge numba numpy pandas matplotlib seaborn arviz pytensor "pymc>=5" jupyter -y
 conda install numpyro -y
 conda install -c conda-forge nutpie -y
