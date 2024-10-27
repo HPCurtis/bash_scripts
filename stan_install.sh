5c94885913eda7bb4d1c9d3e8f0b426cd722a007
Harrison Curtis <harrisoncurtis50@gmail.com>
2024-10-27 20:44:48 +0000

    Add pip isntall brisgestan fro nutpie implementations

--- stan_install.sh
+++ stan_install.sh
@@ -5,3 +5,4 @@ conda activate stan_env
 conda install -c conda-forge numpy patsy scipy pandas matplotlib seaborn arviz jupyter numba -y
 conda install -c conda-forge nutpie -y
 conda install -c conda-forge cmdstanpy -y
+pip install bridgestan
