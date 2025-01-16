5b3fe9f5f67a8a9470ccc69aa7df7eb3ef6d88d3
harrison curtis <harrisoncurtis50@gmail.com>
2025-01-16 17:14:08 +0000

    Remvoe errors

--- pymc_install.sh
+++ pymc_install.sh
@@ -2,13 +2,9 @@
 
 conda create -n pymc_env python=3.11 -y
 conda activate pymc_env 
-<<<<<<< HEAD
-conda install -c conda-forge numba numpy pandas matplotlib seaborn arviz pytensor "pymc>=5" jupyter -y
-conda install numpyro -y
-conda install -c conda-forge nutpie -y
-=======
-conda install -c conda-forge pip numba numpy pandas matplotlib seaborn arviz pytensor jupyter "pymc>=5" bambi nilearn nibabel -y
+
+conda install -c conda-forge pip numba numpy pandas matplotlib seaborn arviz pytensor jupyter "pymc>=5" bambi -y
 conda install conda-forge::numpyro -y
 conda install conda-forge::nutpie -y
 pip install bayeux-ml -y
->>>>>>> 968ddb0 (pymc update)
+
