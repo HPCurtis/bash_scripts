f971b2a922ac350e68b8667f48a270bc78599a34
harrison curtis <harrisoncurtis50@gmail.com>
2025-01-17 10:11:12 +0000

    remove skforcast as versions for other packages cofnlict

--- timeseries_conda.sh
+++ timeseries_conda.sh
@@ -4,7 +4,6 @@ conda activate timeseries_env
 conda install anaconda::statsmodels -y
 conda install conda-forge::darts -y
 conda install conda-forge::statsforecast -y
-pip install skforecast
 conda install -c conda-forge polars
 
  
