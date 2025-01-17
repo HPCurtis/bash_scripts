8f2ddf356259a6d3994882260fa3c31b024dfa60
harrison curtis <harrisoncurtis50@gmail.com>
2025-01-17 10:14:27 +0000

    Add -y flag to bash command for automation

--- timeseries_conda.sh
+++ timeseries_conda.sh
@@ -5,6 +5,6 @@ conda activate timeseries_env
 conda install anaconda::statsmodels -y
 conda install conda-forge::darts -y
 conda install conda-forge::statsforecast -y
-conda install -c conda-forge polars
+conda install -c conda-forge polars -y
 
  
