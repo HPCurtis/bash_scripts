e1f37748366d6785644e39fa54075cd69f1632bf
harrison curtis <harrisoncurtis50@gmail.com>
2025-01-17 10:13:54 +0000

    Update comments

--- timeseries_conda.sh
+++ timeseries_conda.sh
@@ -1,6 +1,7 @@
+
 conda create -n timeseries_env -y
 conda activate timeseries_env
-
+# packages
 conda install anaconda::statsmodels -y
 conda install conda-forge::darts -y
 conda install conda-forge::statsforecast -y
