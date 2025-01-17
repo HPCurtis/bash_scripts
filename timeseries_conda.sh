c49b347c26be54f92b8fa7d0c9904cc9cd7208d0
harrison curtis <harrisoncurtis50@gmail.com>
2025-01-17 10:07:21 +0000

    bashcript to generate timeseries focused conda env

---
+++ timeseries_conda.sh
@@ -0,0 +1,10 @@
+conda create -n timeseries_env -y
+conda activate timeseries_env
+
+conda install anaconda::statsmodels -y
+conda install conda-forge::darts -y
+conda install conda-forge::statsforecast -y
+pip install skforecast
+
+
+ 
